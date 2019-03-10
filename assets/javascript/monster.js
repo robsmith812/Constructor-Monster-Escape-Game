04aa453493fcb0f56f2705772bed20f0cc542cd8
Zamboomafoo <robsmith812@gmail.com>
2019-03-10 11:19:39 -0700

    part 2 complere

--- assets/javascript/monster.js
+++ assets/javascript/monster.js
@@ -1,18 +1,21 @@
 var inquirer = require("inquirer");
 
-function Human(Name) {
+function Human(Name, Health) {
     this.Name = Name;
-    this.Health = 50;
+    this.Health = Health;
 }
 
-function Survivor(Name) {
+function Survivor(Name, Health) {
     this.luckyNum = parseInt(Math.floor(Math.random() * 31));
-    Human.call(this, name);
+    Human.call(this, Name, Health);
 }
 
 Survivor.prototype = Object.create(Human.prototype);
 
 Survivor.prototype.escape = function () {
+
+    var thisSurvivor = this;
+
     return inquirer
         .prompt([
             {
@@ -22,6 +25,75 @@ Survivor.prototype.escape = function () {
             }
         ])
         .then(function (inquirerRespone) {
-            return parseInt(inquirerRespone.number) == this.luckyNum ? true : false;
+            var guess = parseInt(inquirerRespone.number);
+            var val;
+
+            if (guess == thisSurvivor.luckyNum) {
+                console.log('So Lucky')
+                val = true;
+            }
+            else {
+                console.log('Not so Lucky')
+                val = false;
+            }
+            return val;
         })
 }
+
+function Monster(Name, Health, Attack) {
+    this.Attack = Attack;
+    Human.call(this, Name, Health);
+}
+
+Monster.prototype = Object.create(Human.prototype);
+
+Monster.prototype.attack = function () {
+    var ranAttkNum = Math.floor(Math.random() * 6)
+
+    if (ranAttkNum == 3) return false;
+    else return true;
+}
+
+var monsterOne = new Monster('Bahamut', 100, 5);
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is your name?",
+            name: "name",
+        }
+    ])
+    .then(function (inquirerRespone) {
+        var survivor = new Survivor(inquirerRespone.name, 50);
+
+        function callEscape() {
+            survivor.escape().then(function (res) {
+                var esc = res;
+
+                if (esc) {
+                    console.log('You got away!');
+                    return;
+
+                } else {
+                    var a = monsterOne.attack();
+
+                    if (a != false) {
+                        survivor.Health -= monsterOne.Attack;
+
+                        console.log(`You lost ${monsterOne.Attack} health and now you have ${survivor.Health} remaining.`)
+                        return;
+                    } else {
+                        console.log("You dodged the attack!");
+                        return;
+                    }
+                }
+            });
+        }
+
+        setInterval(callEscape, 5 * 1000);
+
+        if (survivor.Health <= 0) {
+            console.log(`${monsterOne.Name} has captured you!`)
+        }
+    });
